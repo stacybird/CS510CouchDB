4e43c43be7d82bd4f827090c9a075464f9a3b1c6
Ryan Niebur <ryanryan52@gmail.com>
2015-06-08 23:04:14 -0700

    allow startKey and endKey

--- CouchTrafficClient/QueryBase.cs
+++ CouchTrafficClient/QueryBase.cs
@@ -63,25 +63,31 @@ namespace CouchTrafficClient
         public abstract string Run();
     private string Server { get { return "http://52.10.252.48:5984/"; } }
 
-        /// <summary>
-        /// Query a view from our CouchDB Server, returning a Dictionary of keys to values!
-        /// </summary>
-        /// <param name="designDocumentName">Name of the Design that the View lives within.</param>
-        /// <param name="viewName">Name of the View that you would like to query.</param>
-        /// <param name="keys">Optional list of keys to query the view for.</param>
-        /// <returns></returns>
+    /// <summary>
+    /// Query a view from our CouchDB Server, returning a Dictionary of keys to values!
+    /// </summary>
+    /// <param name="designDocumentName">Name of the Design that the View lives within.</param>
+    /// <param name="viewName">Name of the View that you would like to query.</param>
+    /// <param name="keys">Optional list of keys to query the view for.</param>
+
+    /// <returns></returns>
     public MultiValueDictionary Query(string designDocumentName, string viewName, IList<object> keys = null, string db = "traffic")
     {
-        dynamic queryResult = InternalQuery(designDocumentName, viewName, keys, db);
-        IList<object> a = queryResult.rows;
-        var result = new MultiValueDictionary();
-        foreach (dynamic data in a)
-        {
-            result.Add(data.key == null ? QueryNullResult : data.key.ToString(), data.value);
-        }
-        return result;
+        return InternalQuery(designDocumentName, viewName, null, null, keys, db);
     }
-    private ExpandoObject InternalQuery(string designDocumentName, string viewName, IList<object> keys = null, string db = "traffic")
+    /// <summary>
+    /// Query a view from our CouchDB Server, returning a Dictionary of keys to values!
+    /// </summary>
+    /// <param name="designDocumentName">Name of the Design that the View lives within.</param>
+    /// <param name="viewName">Name of the View that you would like to query.</param>
+    /// <param name="startKey">Optional list of startkeys to query the view for.</param>
+    /// <param name="endKey">Optional list of endkeys to query the view for.</param>
+    /// <returns></returns>
+    public MultiValueDictionary Query(string designDocumentName, string viewName, object startKey, object endKey, string db = "traffic")
+    {
+        return InternalQuery(designDocumentName, viewName, startKey, endKey, null, db);
+    }
+    private MultiValueDictionary InternalQuery(string designDocumentName, string viewName, object startKey = null, object endKey = null, IList<object> keys = null, string db = "traffic")
     {
         try
         {
@@ -90,14 +96,34 @@ namespace CouchTrafficClient
             {
                 keyString = string.Format("?keys={0}&group=true", Uri.EscapeDataString(JsonConvert.SerializeObject(keys)));
             }
+            else if (startKey != null && endKey != null)
+            {
+                keyString = string.Format("?startkey={0}&endkey={1}&group=true", Uri.EscapeDataString(JsonConvert.SerializeObject(startKey)), Uri.EscapeDataString(JsonConvert.SerializeObject(endKey)));
+            }
+            else if (startKey != null)
+            {
+                keyString = string.Format("?startkey={0}&group=true", Uri.EscapeDataString(JsonConvert.SerializeObject(startKey)));
+            }
+            else if (endKey != null)
+            {
+                keyString = string.Format("?endkey={0}&group=true", Uri.EscapeDataString(JsonConvert.SerializeObject(endKey)));
+            }
             var url = Server + db + "/_design/" + designDocumentName + "/_view/" + viewName + keyString;
+            dynamic queryResult;
             using (WebClient wc = new WebClient())
             {
                 wc.Encoding = System.Text.Encoding.UTF8;
                 wc.Headers["User-Agent"] = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET4.0C; .NET4.0E)";
                 string str = wc.DownloadString(url);
-                return JsonConvert.DeserializeObject<ExpandoObject>(str);
+                queryResult = JsonConvert.DeserializeObject<ExpandoObject>(str);
             }
+            IList<object> a = queryResult.rows;
+            var result = new MultiValueDictionary();
+            foreach (dynamic data in a)
+            {
+                result.Add(data.key == null ? QueryNullResult : data.key.ToString(), data.value);
+            }
+            return result;
         }
         catch (Exception e)
         {
